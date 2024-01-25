955824ec6d2d176c3d829f7eb286198f67992f8f
SHIGE-ZO <147585604+SHIGE-ZO@users.noreply.github.com>
2024-01-25 20:49:50 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP課題007</title>
+</head>
+<body>
+  <?php
+   $product = ['name' => 'onion', 'price'=>200,'weight'=>160];
+   print_r($product);
+  ?>
+</body>
+</html>
